4090d47038d14360a956c3647e5ff9e22c4c789f
Jorge Espina <canchemolinas@gmail.com>
2025-02-22 19:04:48 -0600

    [FEAT]: Theme riverpot

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -13,17 +13,31 @@ const colorList = <Color>[
 ];
 
 class AppTheme {
+  
   final int selectedColor;
+  final bool isDarkmode;
 
-  AppTheme({this.selectedColor = 0}) // aserciones son utiles en la creacion de la aplicacion
-      : assert(selectedColor >= 0, 'Selected color must be greater then 0'),
-        assert(selectedColor < colorList.length , 'Selected color must be less or equal than ${colorList.length - 1}');
+  AppTheme({
+    this.selectedColor = 0,
+    this.isDarkmode = false,
+  }): assert( selectedColor >= 0, 'Selected color must be greater then 0' ),  
+      assert( selectedColor < colorList.length, 
+        'Selected color must be less or equal than ${ colorList.length - 1 }');
 
   ThemeData getTheme() => ThemeData(
     useMaterial3: true,
-    colorSchemeSeed: colorList[selectedColor],
+    brightness: isDarkmode ? Brightness.dark : Brightness.light,
+    colorSchemeSeed: colorList[ selectedColor ],
     appBarTheme: const AppBarTheme(
-      centerTitle: false,
-    )
+      centerTitle: false
+    ),
+  );
+
+  AppTheme copyWith({
+    int? selectedColor,
+    bool? isDarkmode
+  }) => AppTheme(
+    selectedColor: selectedColor ?? this.selectedColor,
+    isDarkmode: isDarkmode ?? this.isDarkmode,
   );
 }
